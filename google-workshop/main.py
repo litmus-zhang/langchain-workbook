78b53226226a9ef068edafdd90341715f932baa8
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-04-19 12:16:16 +0000

    added google workshop

---
+++ google-workshop/main.py
@@ -0,0 +1,12 @@
+import pathlib
+import textwrap
+
+
+import google.generativeai as generativeai
+import google.ai.generativelanguage as glm
+
+from IPython.display import display
+from IPython.display import Markdown
+
+from  google.api_core import retry
+
